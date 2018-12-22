42af10e5dc8bfb4c1f513ed46881122567141933
yxlee <yxlee@shinewave.com>
2018-12-22 15:10:13 +0800

    - deploy

--- src/app/component/book-create/book-create.component.ts
+++ src/app/component/book-create/book-create.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { book } from '../../models/book.model'
@@ -8,24 +8,33 @@ import { book } from '../../models/book.model'
   templateUrl: './book-create.component.html',
   styleUrls: ['./book-create.component.css'],
 })
-export class BookCreateComponent implements OnInit {
+export class BookCreateComponent {
 
-  bookObj: {} = {};
+  bookObj: {
+    "title": string,
+    "isbn": string,
+    "author": string,
+    "publisher": string,
+    "published_year": string
+  } = {
+    "title": "",
+    "isbn": "",
+    "author": "",
+    "publisher": "",
+    "published_year": "",
+  };
 
   constructor(private http: HttpClient, private router: Router) { }
 
-  ngOnInit() {
-  }
-
   saveBook() {
+    console.log(this.bookObj)
     this.http.post('/book', this.bookObj)
       .subscribe(res => {
           let id = res['_id'];
           this.router.navigate(['/book-details', id]);
-        }, (err) => {
-          console.log(err);
-        }
-      );
+      }, (err) => {
+        console.log(err);
+      });
   }
 
 }
