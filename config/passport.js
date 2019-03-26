a94ff546f3fdcfe98cb273e74a5fa86963c2b270
yxlee <yxlee@shinewave.com>
2019-03-26 15:31:16 +0800

    - fix and add vision

---
+++ config/passport.js
@@ -0,0 +1,25 @@
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+// load up the user model
+const User = require('../models/User.model.js');
+
+const config = require('./database');
+
+module.exports = function (passport) {
+  var opts = {};
+  opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
+  opts.secretOrKey = config.secret;
+  passport.use(new JwtStrategy(opts, function (jwt_payload, done) {
+    User.findOne({ id: jwt_payload.id }, function (err, user) {
+      if (err) {
+        return done(err, false);
+      }
+      if (user) {
+        done(null, user);
+      } else {
+        done(null, false);
+      }
+    });
+  }));
+};
