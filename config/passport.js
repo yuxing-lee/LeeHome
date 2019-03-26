dbde211c678c4ad92e8be84968cc0b94fbbd1c11
yxlee <yxlee@shinewave.com>
2019-03-26 15:36:55 +0800

    -fix

--- config/passport.js
+++ config/passport.js
@@ -4,12 +4,10 @@ const ExtractJwt = require('passport-jwt').ExtractJwt;
 // load up the user model
 const User = require('../models/User.model.js');
 
-const config = require('./database');
-
 module.exports = function (passport) {
   var opts = {};
   opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
-  opts.secretOrKey = config.secret;
+  opts.secretOrKey = process.env.secretKey;
   passport.use(new JwtStrategy(opts, function (jwt_payload, done) {
     User.findOne({ id: jwt_payload.id }, function (err, user) {
       if (err) {
